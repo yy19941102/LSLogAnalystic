9f98f71d62b428391edb62433f4480e5ec3145e4
yy19941102 <1797132751@qq.com>
2018-08-19 18:12:47 +0800

    twice commit

---
+++ src/main/java/com/qianfeng/common/GlobalConstants.java
@@ -0,0 +1,15 @@
+package com.qianfeng.common;
+
+public class GlobalConstants {
+    public static final String DEFAULT_VALUE="unknow";
+
+    public static final String ALL_OF_VALUE="all";
+
+    public static final String DRIVER="com.mysql.jdbc.Driver";
+
+    public static final String URL="jdbc:mysql://hadoop01:3306/result";
+
+    public static final String USERNAME="root";
+
+    public static final String PASSWARD="root";
+}
